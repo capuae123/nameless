b4730ce920fcfbaa39ecd76bde3e99a27ff8919c
capuae123 <62066011+capuae123@users.noreply.github.com>
2025-01-25 22:27:19 +0400

    temp bar, boar (0.5)

--- Scripts/GameData.gd
+++ Scripts/GameData.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var energy = 0
+var hot_bubbles = 0
+var cold_bubbles = 0
 
 func add_energy() -> void:
 	if energy < 100:
@@ -12,3 +14,9 @@ func add_energy() -> void:
 func use_energy(n: int) -> void:
 	if n<=energy:
 		energy -= n
+
+func hot_bubble():
+	hot_bubbles += 1
+
+func cold_bubble():
+	cold_bubbles += 1

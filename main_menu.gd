332dd0a9ce7491e19dace1a063242ae694dae87d
capuae123 <62066011+capuae123@users.noreply.github.com>
2025-01-26 16:08:51 +0400

    Fix Menu Transition

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/intro_scene.tscn")
 
 
 func _on_options_pressed() -> void:

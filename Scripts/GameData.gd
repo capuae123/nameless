d6ca1cffcd2697f878b6d94a3c69d593946b05e1
Croolsce <139340426+Croolsce@users.noreply.github.com>
2025-01-26 04:39:57 +0400

    cah

--- Scripts/GameData.gd
+++ Scripts/GameData.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal temperature_changed(new_temp: int)  # Declare signal at the top level
+
 var energy = 0
 var temp = 0
 var hot_bubbles = 0
@@ -14,28 +16,33 @@ func add_energy() -> void:
 
 # Consumes energy by N amount
 func use_energy(n: int) -> void:
-	if n<=energy:
+	if n <= energy:
 		energy -= n
 
 func hot_bubble():
 	hot_bubbles += 1
+	emit_temperature_changed()
 
 func cold_bubble():
 	cold_bubbles += 1
+	emit_temperature_changed()
 
 func electric_bubbles():
 	electric_bubble += 1
 
-func get_temp():
+func get_temp() -> int:
 	temp = hot_bubbles - cold_bubbles
-	if temp <= 55 and temp >= -55:
-		return temp
-	else:
-		if temp > 55:
-			temp = 55
-		elif temp < -55:
-			temp = -55
-		return temp
+	temp = clamp(temp, -55, 55)  # Clamp the temperature within -55 to 55
+	return temp
 
-func change_temp(n):
+func change_temp(n: int) -> void:
 	temp += n
+	temp = clamp(temp, -55, 55)  # Clamp the temperature within -55 to 55
+	emit_temperature_changed()
+	
+
+# Emit the temperature_changed signal
+func emit_temperature_changed() -> void:
+	temp = get_temp()
+	emit_signal("temperature_changed", temp)
+	print(temp)
